f2c98ca3093c5efb8ab9d96aea4ff6baccd2f822
Eraldo <hama.eraldo@gmail.com>
2017-05-02 13:50:27 +0200

    main page

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import json
+
+# Create the app var to run as a webserver for local development
+app = Flask(__name__)
+
+# This defines the index page returning just a string.
+# app.route bind the url typed in the browser with the method main_page
+
+@app.route("/")
+def main_page():
+	return json.dumps([{"nome":"Eraldo","cognome":"Hamataj"}, {"file":"index.html"}])
+	
+
+# Run the webserver in localhost at port 5000 - default
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
